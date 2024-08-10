d69267eeb3404c8a79087e78636c884fa64728de
DCoder53 <158759303+DCoder53@users.noreply.github.com>
2024-08-10 19:01:16 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,11 @@
 // middleware.ts
 import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware(() => {
-  console.log("Clerk middleware is running");
-});
+export default clerkMiddleware();
 
-// This will ensure that all routes use the Clerk middleware
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 };
+
