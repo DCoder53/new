5135e66d371939e4630c12a0f9f1328472096743
DCoder53 <158759303+DCoder53@users.noreply.github.com>
2024-08-10 23:35:41 +0530

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,12 @@
-// middleware.ts
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// // middleware.ts
+// import { clerkMiddleware } from '@clerk/nextjs/server';
 
-export default clerkMiddleware({
-  secretKey: process.env.CLERK_SECRET_KEY,
-});
+// export default clerkMiddleware({
+//   secretKey: process.env.CLERK_SECRET_KEY,
+// });
 
-export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
-  ],
-};
+// export const config = {
+//   matcher: [
+//     '/((?!api|_next/static|_next/image|favicon.ico).*)',
+//   ],
+// };
